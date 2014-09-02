88d2ecb64db4d1d86ce37d439a512d5e445cb86a
Kyuuri <dark_e2102@hotmail.com>
2014-09-02 15:31:34 +0700

    Add link, improve ui.

--- src/View/UI.java
+++ src/View/UI.java
@@ -1,13 +1,23 @@
 package View;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Cursor;
+import java.awt.Desktop;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.Graphics;
 import java.awt.GridLayout;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.io.IOException;
 import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 
 import javax.swing.JButton;
@@ -18,6 +28,7 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.border.Border;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 
@@ -27,13 +38,19 @@ import Model.Item;
 public class UI extends JFrame{
 	
 	private RSSReader rssReader;
-	private JTextField inputField;
 	private JPanel panel;
 	private JPanel topPanel;
 	private JPanel innerTopPanelUp;
 	private JPanel innerTopPanelDown;
+	
+	private JLabel strInput;
+	private JTextField inputField;
 	private JButton btnRead;
 	private JPanel centerPanel;
+	
+	private JPanel bottomPanel;
+	private JLabel linkText;
+	private String link = "";
 	
 	private JLabel urlStr;
 	private JLabel titleStr;
@@ -52,11 +69,17 @@ public class UI extends JFrame{
 		innerTopPanelUp = new JPanel();
 		innerTopPanelUp.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 		
+		strInput = new JLabel("URL : ");
+		strInput.setFont( new Font("Arial", Font.BOLD, 16));
+		innerTopPanelUp.add(strInput);
+		
 		//default url
-		urlStr = new JLabel("<html>Current URL : " + colorText("http://feeds.bbci.co.uk/news/rss.xml") + "</html>");
+		urlStr = new JLabel("<html>Current URL : " + colorText("http://feeds.bbci.co.uk/news/rss.xml", "red") + "</html>");
+		urlStr.setFont( new Font("Tahoma", Font.PLAIN, 16));
 		urlStr.setHorizontalAlignment(JLabel.CENTER);
 		
 		titleStr = new JLabel("Title : None");
+		titleStr.setFont( new Font("Tahoma", Font.PLAIN, 16) );
 		titleStr.setHorizontalAlignment(JLabel.CENTER);
 		
 		topPanel = new JPanel();
@@ -92,7 +115,7 @@ public class UI extends JFrame{
 				itemList.setListData( rssReader.getArrayItem() );
 				information.setText("");
 				titleStr.setText("Title : None");
-				urlStr.setText("<html>Current URL : " + colorText(url.toString()) + "</html>");
+				urlStr.setText("<html>Current URL : " + colorText(url.toString(), "red") + "</html>");
 			}
 		});
 		innerTopPanelUp.add(btnRead);
@@ -100,6 +123,7 @@ public class UI extends JFrame{
 		centerPanel = new JPanel();
 		
 		itemList = new JList( rssReader.getArrayItem() );
+		itemList.setFont( new Font("Arial", Font.BOLD, 14) );
 		itemListPane = new JScrollPane(itemList);
 		
 		information = new JTextArea();
@@ -107,8 +131,8 @@ public class UI extends JFrame{
 		
 		informationPane = new JScrollPane(information);
 		
-		itemListPane.setPreferredSize( new Dimension(400,300) );
-		informationPane.setPreferredSize( new Dimension(400,300) );
+		itemListPane.setPreferredSize( new Dimension(500,400) );
+		informationPane.setPreferredSize( new Dimension(500,400) );
 		
 		itemList.addListSelectionListener(new ListSelectionListener() {			
 			@Override
@@ -117,13 +141,51 @@ public class UI extends JFrame{
 				if(e.getValueIsAdjusting()){
 					Item item = (Item)(itemList.getSelectedValue());
 					information.setText(item.getDesc());
-					information.setLineWrap(true);
 					
-					titleStr.setText("<html>Title : " + colorText(item.getTitle()) + "</html>");
+					titleStr.setText("<html>Title : " + colorText(item.getTitle(), "blue") + "</html>");
+					link = item.getLink();
 				}
 				
 			}
 		});
+		
+		linkText = new JLabel("More detail...");
+		linkText.setHorizontalAlignment(JLabel.CENTER);
+		linkText.addMouseListener( new MouseListener() {
+			
+			@Override
+			public void mouseReleased(MouseEvent e) {}
+			
+			@Override
+			public void mousePressed(MouseEvent e) {
+				if (Desktop.isDesktopSupported() && !link.equals("")) {
+					Desktop desktop = Desktop.getDesktop();
+					try {
+						desktop.browse(new URI(link));
+					} catch (IOException e1) {
+						e1.printStackTrace();
+					} catch (URISyntaxException e1) {
+						e1.printStackTrace();
+					}
+				}
+				
+			}
+			
+			@Override
+			public void mouseExited(MouseEvent e) {}
+			
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				linkText.setCursor( new Cursor(Cursor.HAND_CURSOR) );
+			}
+			
+			@Override
+			public void mouseClicked(MouseEvent e) {}
+		});
+		
+		bottomPanel = new JPanel( new GridLayout(2,2));
+		bottomPanel.add( new JLabel() );
+		bottomPanel.add(linkText);
 		
 		topPanel.add(innerTopPanelUp,BorderLayout.NORTH);
 		topPanel.add(innerTopPanelDown,BorderLayout.SOUTH);
@@ -132,8 +194,9 @@ public class UI extends JFrame{
 		centerPanel.add(informationPane, BorderLayout.EAST);
 		
 		panel.add(topPanel, BorderLayout.NORTH);
-		//panel.add()
 		panel.add(centerPanel, BorderLayout.CENTER);
+		panel.add(bottomPanel, BorderLayout.SOUTH);
+
 		this.getContentPane().add(panel);
 
 	}
@@ -142,17 +205,19 @@ public class UI extends JFrame{
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.pack();
 		this.setVisible(true);
+		this.setResizable(false);
 		System.out.println("Runn");
 	}
 	
 	public void setInformationArea(){
 		information.setEditable(false);
+		information.setLineWrap(true);
 		information.setWrapStyleWord(true);
 		information.setFont( new Font("Tahoma",Font.PLAIN,16));
 	}
 	
-	public String colorText(String text){
-		return "<font color=\"blue\">" + text + "</font>";
+	public String colorText(String text , String color){
+		return "<font color=\"" + color + "\">" + text + "</font>";
 	}
 	
 	
